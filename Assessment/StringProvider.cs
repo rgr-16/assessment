1e85ad9dd7799d55f7112fa48205efbdb2f39f7f
rgr-16 <educarte16sep@gmail.com>
2021-08-23 20:06:00 -0400

    Adding suport whit other separator

--- Assessment/StringProvider.cs
+++ Assessment/StringProvider.cs
@@ -4,12 +4,25 @@ namespace Assessment
 {
     public class StringProvider : IElementsProvider<string>
     {
-        private readonly string separator = ",";
-
-
         public IEnumerable<string> ProcessData(string source)
         {
-            return source.Split(separator);
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<string> ProcessDataComa(string source)
+        {
+    
+            return source.Split(SeparatorChars.Comma);
+        }
+
+        public IEnumerable<string> ProcessDataPipe(string source)
+        {
+            return source.Split(SeparatorChars.Pipe);
+        }
+
+        public IEnumerable<string> ProcessDataSpace(string source)
+        {
+            return source.Split(SeparatorChars.Space);
         }
     }
 }
